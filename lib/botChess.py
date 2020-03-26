0d80197a2118665b7085a9969b47ce1f2c4af89d
Waine Junior <waine2000@gmail.com>
2020-03-25 22:10:03 -0300

    Updated botChess

--- lib/botChess.py
+++ lib/botChess.py
@@ -20,26 +20,26 @@ class BotChess:
         self.lock_ongoing_games = Lock()
 
         self.game_states = {}
-        self.game_move_votes = {}
+        self.lock_game_states = {}
 
         ret = self.start_session()
         if(ret is None):
             raise Exception(
                 'Unable to connect to lichess API. Check your personal token')
 
-        self.board = chess.Board()
         self.update_ongoing_games()
 
     def thread_update_ongoing_games(self):
         while(True):
-            time.sleep(5)
             self.update_ongoing_games()
+            time.sleep(5)
 
     def thread_stream_game_states(self):
         while(True):
             for game_id in self.get_ongoing_games_ids():
                 if(game_id not in self.game_states.keys()):
                     self.stream_game_state(game_id)
+            time.sleep(5)
 
     def vote_for_move(self, game_id, move):
         if(game_id not in self.game_move_votes.keys()):
@@ -76,22 +76,37 @@ class BotChess:
         except Exception as e:
             print_debug(str(e), 'EXCEPTION')
 
-    def get_is_move_valid(self, move):
-        # board.legal_moves prints <LegalMoveGenerator at 0x7ff666656908 (Nh3, ...)>
-        return move in str(self.board.legal_moves).split("(")[-1].split(")")[0]
+    def get_is_move_valid(self, move, game_id=None):
+        if(game_id is None or game_id not in self.game_states.keys()):
+            if(game_id not in self.game_states.keys()):
+                print_debug(f'State of game {game_id} not available to ' +
+                            'get is move valid')
+            # board.legal_moves prints <LegalMoveGenerator at 0x7ff666656908 (Nh3, ...)>
+            board = chess.Board()
+        else:
+            # TODO: load from game state
+            board = chess.Board()
+
+        return move in str(board.legal_moves).split("(")[-1].split(")")[0]
 
     def stream_game_state(self, game_id):
         # USE AS THREAD
+        self.lock_game_states[game_id] = Lock()
+        self.game_states[game_id]['board'] = chess.Board()
         try:
-            self.game_states[game_id]['lock'] = Lock()
             for event in self.client.bots.stream_game_state(game_id):
-                with self.game_states[game_id]['lock']:
+                with self.lock_game_states[game_id]:
                     self.game_states[game_id]['game_state'] = event
-            with self.game_states[game_id]['lock']:
+                    self.game_states[game_id]['board'] = event
+                    
+            with self.lock_game_states[game_id]:
                 del self.game_states[game_id]
+            del self.lock_game_states[game_id]
+
             return True
         except Exception as e:
             if(game_id in self.game_states.keys()):
+
                 del self.game_states[game_id]
             print_debug(str(e), 'EXCEPTION')
             return None
